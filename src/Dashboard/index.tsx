9d735e7963dea004a1c31ec3fd37046fa2297cee
alfredo chivela <alfredochivela3@gmail.com>
2024-12-19 00:13:48 +0100

    build layout list repository

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -1,9 +1,56 @@
 import React from "react";
-import { Title } from "./style";
+import { Form, Title, Repositories } from "./style";
+import { FiChevronRight } from "react-icons/fi";
+import logoImg from "../assets/github_explore.svg";
 
-const Dashboard:React.FC = () =>
-{
-    return <Title>Explore repositorios no!</Title>
-}
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="" />
+      <Title>Explore repositorios no GitHub!</Title>
 
-export default Dashboard
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button>Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/59560615?v=4"
+            alt="Alfredo Manuel"
+          />
+          <div>
+            <strong>Alfredo003/angola</strong>
+            <p>easy peasy highly scalable react.js </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/59560615?v=4"
+            alt="Alfredo Manuel"
+          />
+          <div>
+            <strong>Alfredo003/angola</strong>
+            <p>easy peasy highly scalable react.js </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/59560615?v=4"
+            alt="Alfredo Manuel"
+          />
+          <div>
+            <strong>Alfredo003/angola</strong>
+            <p>easy peasy highly scalable react.js </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
