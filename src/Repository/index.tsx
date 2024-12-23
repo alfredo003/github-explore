df910cd8f207cc3fdf022833b6c0f5ae96ecae8e
alfredo chivela <alfredochivela3@gmail.com>
2024-12-23 16:37:22 +0100

    buiid layout repository

--- src/Repository/index.tsx
+++ src/Repository/index.tsx
@@ -1,15 +1,58 @@
 import React from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
+import { Header, Issues, RepositoryInfo } from "./style";
+import logoImg from "../assets/github_explore.svg";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
+const Respository: React.FC = () => {
+  const params = useParams();
 
-const Respository:React.FC = () =>
-{
-    const params = useParams();
+  const repository = `${params["repository"]}/${params["*"]}`;
 
-    const repository = `${params['repository']}/${params['*']}`;
- 
-
-    return <h1>Respository:{repository}</h1>;
-}
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="github explore" />
+        <Link to="/">
+          {" "}
+          <FiChevronLeft /> Voltar{" "}
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/59560615?v=4"
+            alt=""
+          />
+          <div>
+            <strong>alfredo003/minischell</strong>
+            <p>Descricao do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1000</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1000</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1000</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link key="fg" to={`//`}>
+          <strong>oooooooo</strong>
+          <p>ppppppppppppp</p>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
 
 export default Respository;
